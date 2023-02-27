e312ae2bfd831392cdc4cca07534c59e8f382690
Waldemar Galizzi <86132080+Wallesi@users.noreply.github.com>
2023-02-27 12:13:09 -0300

    Add react proyects

--- src/app/componentes/education/education.component.ts
+++ src/app/componentes/education/education.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
-import { ComunicarService } from 'src/app/servicios/comunicar.service';
+
 
 @Component({
   selector: 'app-education',
@@ -8,20 +8,21 @@ import { ComunicarService } from 'src/app/servicios/comunicar.service';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
-  showPencilEducacion!: boolean;
+ 
   miEducation:any;
-  constructor(private datosPorfolio:PortfolioService, public comunicarComponentes:ComunicarService) { }
+  mostrarBoton=true;
+  constructor(private datosPorfolio:PortfolioService) { }
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data=>{
       
       this.miEducation=data.educacion;
-    })
-   
-    console.log('Estamos parados en educacion',this.comunicarComponentes.mostrarLapiz);
 
-    if(this.comunicarComponentes.mostrarLapiz===true){
-      this.showPencilEducacion=true;
-    }
+      if(this.miEducation=data.education.certificado===null){
+        this.mostrarBoton=false;
+        console.log("Entra aca");
+      }
+    })
+    
   }
 }
